cd1937837030d51efc3482f97e6f2fe67d1d9026
Kelley <kbonneville@cnm.edu>
2021-07-14 11:59:14 -0600

    Google sign-in progress.

--- app/src/main/java/edu/cnm/deepdive/googlesignindemo/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/googlesignindemo/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package controller;
+package edu.cnm.deepdive.googlesignindemo.controller;
 
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
